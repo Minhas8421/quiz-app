2b51de769677d3f0dd9e1da809548c6b73b9e96f
Minhas-Kharal <85873812+Minhas-Kharal@users.noreply.github.com>
2024-09-29 14:14:37 +0500

    Hello

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,108 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useState } from "react";
+import questions from "./data/questions.json";
+import styles from "./styling/stye.module.css";
 
-export default function Home() {
+
+export default function QuizApp() {
+  let [currentQuestIndex, setCurrentQuestIndex] = useState(0);
+  let [selectedOptionIndex, setSelectedOptionIndex] = useState(null);
+  let [obtainScore, setObtainScore] = useState(0);
+
+  const onOptionSelected = (selectedOptionIndex) => {
+    setSelectedOptionIndex(selectedOptionIndex);
+
+    if (selectedOptionIndex === questions[currentQuestIndex].correctOptionIndex) {
+      setObtainScore(obtainScore + 1);
+
+    }
+    else {
+
+    }
+    setTimeout(function () {
+      setCurrentQuestIndex(currentQuestIndex + 1);
+      setSelectedOptionIndex(null);
+    }, 300);
+  };
+
+  const nextQuestion = () => {
+    if (selectedOptionIndex == null) {
+      alert("Please select an option")
+    } else {
+      setSelectedOptionIndex(null);
+      setCurrentQuestIndex(currentQuestIndex + 1);
+    }
+  }
+
+  const restart = () => {
+    setCurrentQuestIndex(0);
+    setObtainScore(0);
+  }
+
+  if (currentQuestIndex === questions.length) {
+    return (
+      <>
+        <div className="container my-5">
+          <div className="row">
+            <div className="col-md-3 col-sm-3"></div>
+            <div className="col-md-6 col-sm-6">
+              <div className="card text-center">
+                <div className="card-header">
+                  <h1>Quiz Finished !</h1>
+                </div>
+                <div className="card-body score">
+                  <h5 className="card-title">Score Board</h5>
+                  <p className="card-text score">{obtainScore} / {questions.length}</p>
+                  <button onClick={restart} className="btn btn-primary">Restart</button>
+                </div>
+              </div >
+            </div>
+            <div className="col-md-3 col-sm-3"></div>
+          </div>
+        </div >
+      </>
+    )
+  }
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-md-3 col-sm-3"></div>
+          <div className="col-md-6 col-sm-6">
+            <div className="card text-center">
+              <div className="card-header">
+                <h3>Question # {questions[currentQuestIndex]?.id}</h3>
+              </div>
+              <div className="card-body">
+                <h5 className="card-title">{questions[currentQuestIndex]?.statement}</h5>
+                <ul className="list-group">
+                  {questions[currentQuestIndex]?.options.map((option, index) => (
+                    <li
+                      // className={selectedOptionIndex === index ? "list-group-item active" : "list-group-item"}
+                      onClick={() => onOptionSelected(index)}
+                      className={styles['list-group-item']}
+                    // className={`$(styles['list-group-item'] $(styles['list-group-item:hover']`}
+                    // style={{
+                    //   backgroundColor: index === questions[currentQuestIndex].correctOptionIndex ? 'green' : index !== questions[currentQuestIndex].correctOptionIndex ? 'red' : 'black',
+                    //   cursor: 'pointer',
+                    //   margin: '5px 0',
+                    // }}
+                    >
+                      {option}
+                    </li>
+                  ))}
+                </ul>
+                <button onClick={nextQuestion} className="btn btn-primary mt-3">Next</button>
+              </div>
+              <div className="card-footer text-body-secondary">
+                {questions[currentQuestIndex]?.id}/{questions.length}
+              </div>
+            </div >
+          </div>
+          <div className="col-md-3 col-sm-3"></div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div >
+    </>
+  )
 }
